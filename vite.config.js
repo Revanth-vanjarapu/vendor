a24bd0dc498969b9f5220f5c3e854a20a95f5cb0
vsaas-technologies-company <46972569+Revanth-vanjarapu@users.noreply.github.com>
2025-12-27 12:56:57 +0530

    for production safe file

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  // 🔴 REQUIRED for /vendor deployment
-  base: "/vendor/",
-
   plugins: [react()],
-});
+  base: '/',   // ✅ IMPORTANT
+})
